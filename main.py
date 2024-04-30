c6f25f61b24f3b18dd369ede31a02e9174b2949d
arapovad <lescha05012006@mail.ru>
2024-04-30 13:43:22 +0400

    final 1

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import sys
 import os
 import time
-
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QInputDialog
-from ui_dialog import Ui_Dialog
+from PyQt6.QtGui import QPixmap, QTransform
+from PyQt6.QtWidgets import QApplication, QMainWindow, QInputDialog
 from ui_Main import Ui_MainWindow
-import threading
 
-# event = threading.Event()
-# path = ''
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -19,6 +14,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.im_list = []
         self.list_i = 0
         self.list_l = 0
+        self.turn_angle = 90
         self.btn_disable()
 
         # кнопки
@@ -28,14 +24,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.first_btn.clicked.connect(self.buttons)
         self.last_btn.clicked.connect(self.buttons)
         self.slide_show_btn.clicked.connect(self.buttons)
-        self.turn_l_btn.clicked.connect(self.buttons)
-        self.turn_r_btn.clicked.connect(self.buttons)
+        self.turn_btn.clicked.connect(self.buttons)
         self.del_bnt.clicked.connect(self.buttons)
 
     def buttons(self):
         if self.sender() == self.folder_btn:
-            # self.dialog = Dialog()
-            # self.dialog.show()
             self.path, ok = QInputDialog.getText(self, 'Выбор папки', 'Введите адрес папки: ')
             self.set_folder()
         if self.sender() == self.prev_btn:
@@ -54,16 +47,23 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.list_i = self.list_l
             self.setpix(self.im_list[self.list_l])
             self.btn_disable()
+        if self.sender() == self.slide_show_btn:
+            while self.list_i != self.list_l + 1:
+                self.setpix(self.im_list[self.list_i])
+                self.btn_disable()
+                self.list_i += 1
+                time.sleep(5)
+        if self.sender() == self.turn_btn:
+            self.turnpix()
+
 
     def set_folder(self):
         os.chdir(self.path)
         self.link.setText(os.getcwd())
         self.im_list = os.listdir()  # лист со всеми названиями изображений
-        print(self.im_list)
         self.list_l = len(self.im_list) - 1
         self.btn_disable()
         self.setpix(self.im_list[self.list_i])
-        self.show()
 
     def btn_disable(self):
         if self.list_i == 0:
@@ -75,36 +75,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if self.list_i == self.list_l:
             self.next_btn.setDisabled(1)
             self.last_btn.setDisabled(1)
+            self.slide_show_btn.setDisabled(1)
         else:
             self.next_btn.setDisabled(0)
             self.last_btn.setDisabled(0)
+            self.slide_show_btn.setDisabled(1)
 
     # ставит изображение по названию
     def setpix(self, filename):
         print(f"Изображение {filename} поставлено")
-        self.pixmap = QPixmap(filename)
-        # self.pixmap.scaled(1260, 601, transformMode=)
-        # self.pixmap.transformed(transform=)
-        self.image.setPixmap(self.pixmap)
+        pixmap = QPixmap(filename)
+        self.image.setPixmap(pixmap)
         self.name.setText(filename)
 
+    def turnpix(self):
+        pixmap = QPixmap(self.im_list[self.list_i])
+        transform90 = QTransform().rotate(self.turn_angle)
+        self.turn_angle += 90
+        rotated = pixmap.transformed(transform90)
+        pixmap = QPixmap(rotated)
+        self.image.setPixmap(pixmap)
 
-# class Dialog(QDialog, Ui_Dialog):
-#     def __init__(self):
-#         super().__init__()
-#         self.setupUi(self)
-#
-#
-#         self.ok.clicked.connect(self.buttons)
-#         self.cancel.clicked.connect(self.buttons)
-#
-#     def buttons(self):
-#         if self.sender() == self.ok:
-#             path = self.line.text()
-#             self.close()
-#
-#         if self.sender() == self.cancel:
-#             self.close()
 
 
 if __name__ == '__main__':
